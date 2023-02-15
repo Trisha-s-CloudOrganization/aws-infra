6ae5c09b1c780c9267ef1548800f3e3aec2e2fc2
ghorpade.t <ghorpade.t@northeastern.edu>
2023-02-15 17:17:39 -0500

    vpc config created

---
+++ terraform/env/variables.tf
@@ -0,0 +1,16 @@
+variable "cidr_name" {
+  description = "cidr block name"
+  type        = string
+}
+variable "vpc_tag_name" {
+  description = "Vpc name"
+  type        = string
+}
+variable "aws_profile" {
+  description = "aws profile specification"
+  type        = string
+}
+variable "aws_region" {
+  description = "aws region"
+  type        = string
+}
