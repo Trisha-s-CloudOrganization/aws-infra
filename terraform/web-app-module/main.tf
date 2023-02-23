1d4d7f6827587a3ba80e1891c8e52db24dccf298
Trisha Ghorpade <98073016+ghorpadetrisha16@users.noreply.github.com>
2023-02-23 16:57:23 -0500

    Update main.tf

--- terraform/web-app-module/main.tf
+++ terraform/web-app-module/main.tf
@@ -49,6 +49,7 @@ resource "aws_subnet" "public_subnet" {
   cidr_block        = cidrsubnet(aws_vpc.webapp_vpc.cidr_block, 8, count.index)
   vpc_id            = aws_vpc.webapp_vpc.id
   availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet-${aws_vpc.webapp_vpc.id}-${count.index + 1}"
   }
